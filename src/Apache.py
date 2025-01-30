7e7659a6c2013fb92036fb19d44196a56c61009a
notevensmart <parth.college123@gmail.com>
2025-01-29 21:41:04 -0500

    server stuff

--- src/Apache.py
+++ src/Apache.py
@@ -29,7 +29,7 @@ def get_transform():
 def load_model():
     torch.serialization.add_safe_globals([ResNetForImageClassification])
     model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
-    model.load_state_dict(torch.load(MODEL_PATH, map_location="cpu", weights_only= False))
+    model.load_state_dict(torch.load(MODEL_PATH, map_location="cpu"))
     model.eval()
     return model
 
